f5d8f27f9fc1aa7579761cca721da6574aeda9c5
Courtney1723 <92752814+Courtney1723@users.noreply.github.com>
2022-05-21 02:18:49 +0000

    fixed rdo and gta commands

--- commands/GTAcommand.js
+++ commands/GTAcommand.js
@@ -14,11 +14,19 @@ module.exports = {
 
     if (!message.content.toLowerCase().startsWith(prefix + 'gta')) return;
 
-  const gta1 =   await fetch('https://www.reddit.com/r/gtaonline.json').then(res => res.text())
+  const gta1 =   await fetch('https://www.ign.com/wikis/gta-5/GTA_Online_Weekly_Updates').then(res => res.text())
 
-  let args = gta1.split("**Podium Vehicle**") //Gets the latest post
+  let args = gta1.split("mw-headline") 
 
-  let gtaBody = args[0].split("Thanks to")//Gets the latest post
+  let gtaBody = args[2].split("2022: ")//Gets the latest post
+
+  let gtaTitle = args[2].split("\"header\\\"\\u003e")
+
+  let gtaTitle1 = gtaTitle[1].split(":")
+
+  let gtaBodyMain = gtaBody[1].split("u003e\\u003cli\\u003e\\u003cb\\u003e") //Gets the body of the post
+
+  let gtaBodyMain1 = gtaBodyMain[1].split("Prime Gaming accounts by visiting")
 
   const aDate = new Date();
 
@@ -29,19 +37,22 @@ module.exports = {
   let dayDate = aDate.getDate();
 
   if (message.content.toLowerCase().startsWith(prefix + "gta")) {
+    
+        message.channel.send({embed: {
+            color: 0xB75AFF, //Purple
+            title: `${monthDate} ${dayDate}, ${yearDate} - GTA Online Weekly Bonuses & Discounts:`,
+           description: `**Last Updated ${gtaTitle1[0]}** \n\n${gtaBodyMain1[0]} by [clicking here](https://Twitch.amazon.com/Prime/Loot/GTAonline)`
 
-        //message.channel.send({embed: {
-            //color: 0xB75AFF, //Purple
-            //title: `${monthDate} ${dayDate}, ${yearDate} - GTA Online Weekly Bonuses & Discounts:`,
-           //description: 
-    console.log(`**Podium Vehicle** - ${gtaBody[0]} \n[Click Here for more Info](https://www.ign.com/wikis/gta-5/GTA_Online_Weekly_Updates)`
-            .replace(/&amp;#x200B;/g ,"_ _")
-            .replace(/\\u00dc/g ,"Ü")//For the Übermacht
-            .replace(/\\n\\n/g ," \n ") // proper spacing
-            .replace(/\\\\- /g ," ")
-            .replace(/&amp;/g ,"&")
+      .replace(/\\u0026amp;/g, "&") // &
+      .replace(/\\u0026#x2019;/g, "'") // '
+      .replace(/\\u003c\/b\\u003e\\u003cbr\\u003e/g, "\n")
+.replace(/\\u003c\/li\\u003e\\n\\u003cli\\u003e\\u003cb\\u003e/g, "\n\n")
+      .replace(/\\u003cbr\\u003e/g, "\n")
+      .replace(/\\u003c\/b\\u003e/g, "")
 
-        )    
+    }})
+    
+      
             
     
             
--- commands/RDOcommand.js
+++ commands/RDOcommand.js
@@ -15,31 +15,44 @@ module.exports = {
 
   const rdo1 =   await fetch('https://www.ign.com/wikis/red-dead-redemption-2/Red_Dead_Online_Updates_Archive#Red_Dead_Redemption_2_Online_Updates').then(res => res.text())
 
-  let args = rdo1.split("mw-headline").slice(2) // gets latest post
+  let args = rdo1.split("\"header\\\"\\u003e") // gets latest post
 
-  let rdoDate = args[0].split(">") //Gets the Date
+    let rdoBody = args[3].split("\\u003c\/li\\u003e\\u003c\/")
 
-  let rdoTitles = args[2].split("<b>").slice(1).join("\n\n")//sections
+    let rdoBody1 = rdoBody[0].split("\"html\":\"\\u003cul\\u003e\\u003cli\\u003e \\u003cb\\u003e")
+
+    let rdoMonthBody = args[4].split("\\u003c\/li\\u003e\\u003c\/")
+
+    let rdoMonthBody1 = rdoMonthBody[0].split("{\"html\":\"\\u003cul\\u003e\\u003cli\\u003e \\u003cb\\u003e")
+
+  let rdoDate = args[3].split("\\u003c\/span\\") //Gets the Date
+
 
 
   if (message.content.toLowerCase().startsWith(prefix + "rdo")) {
 
+  
     message.channel.send({embed: {
             color: 0xB75AFF, //Purple
-            title: `${rdoDate[1]}`
-            .replace(/Updates<\/span/g ," "),
-            description: `${rdoTitles} \n[Click Here for more info](https://www.ign.com/wikis/red-dead-redemption-2/Red_Dead_Online_Updates_Archive)`
-            .replace(/<\/b>/g ," ")
-            .replace(/<\/li>/g ," ")
-            .replace(/<li>/g ," ")
-            .replace(/<br\/>/g ,"\n")
-            .replace(/<\/ul>/g ," ")
-            .replace(/<\/section>/g ,"\n\n ")
-            .replace(/<section class="/g ," ")
-            .replace(/jsx-/g ,"")
-            .replace(/wiki-section wiki-html">/g ,"")
-            .replace(/<h2>/g ,"")
-            .replace(/<span class="/g ,"")
-            }})}}}
+            title: `Red Dead Online ${rdoDate[0]}`,
+            description: `${rdoBody1[1]}\n\n**Monthly Bonuses:**\n${rdoMonthBody1[1]} \n[Click Here for more info](https://www.ign.com/wikis/red-dead-redemption-2/Red_Dead_Online_Updates_Archive)`
+            .replace(/\\u0026#x2019;/g , "'")
+            .replace(/\\u003c\/b\\u003e\\u003cbr\\u003e/g, "\n")
+            .replace(/\\u003cbr\\u003e/g, "\n")
+            .replace(/\\u003c\/li\\u003e\\n\\u003cli\\u003e \\u003cb\\u003e/g, "\n\n")
+            .replace(/\\u0026#x2018;/g, "'")
+
+                                    
+                        
+                        
+                        
+                        
+                        }})
+    
+    
+
+
+
+  }}}
           
 
